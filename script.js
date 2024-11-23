8d999747a63cc1eaf7cbd728bbac8257c4cc0c35
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-23 17:23:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+
+  let l1=s1.length;
+  let l2=s2.length;
+
+	if(l1<l2)
+	{
+		return -1;
+	}
+	let ans=-1;
+	let flag=0;
+
+  for(let i=0;i<l1;)
+	  {
+		  if(s1.charAt(i).toUpperCase()===s2.charAt(0).toUpperCase())
+		  {
+			  let j=0;
+			  let p=i;
+			  while(i<l1 && j<l2 && s1.charAt(i).toUpperCase()===s2.charAt(0).toUpperCase())
+				  {
+					  i++;
+					  j++;
+				  }	 
+			  if(j>l2)
+			  {
+				  ans=p;
+				  break;
+			  }
+		  }
+		  else
+		  {
+			  i++;
+		  }
+	  }
+	return ans;
 }
 
 // Please do not change the code below
